e0015166efc0a5f316545610f6b052b486245952
Axel claver <axel@axflash.com>
2016-09-21 01:04:42 +0200

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
-    console.log('user disconnected :(');
+    console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);

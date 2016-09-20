ab3c0b7230cdf46d3a1ed875fbb5c1d5e2927726
Axel claver <axel@axflash.com>
2016-09-20 19:54:53 +0200

    Demo

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+http.listen(5000, function(){
+  console.log('listening on *:3000');
 });
-
-

66d6eb3d77243f707dd3ff569a46142cc726fdfa
Axel claver <axel@axflash.com>
2016-09-20 20:27:20 +0200

    Demo

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+  //response.render('pages/index');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(5000, function(){
-  console.log('listening on *:5000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
+

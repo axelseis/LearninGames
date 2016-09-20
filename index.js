e6ee152f0e0da5d5e6993e2f7d0196d7033667ac
Axel claver <axel@axflash.com>
2016-09-20 20:22:29 +0200

    Demo

--- index.js
+++ index.js
@@ -2,23 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000));
-
-/*
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-*/
-
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
-  //response.render('pages/index');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
-
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});

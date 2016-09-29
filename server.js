c3d2d71986be1af75a44359d57b430081569893d
Axel claver <axel@axflash.com>
2016-09-29 17:24:15 +0200

    reordenando files

--- server.js
+++ server.js
@@ -177,67 +177,10 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
+
 //===============ROUTES===============
+require('./app/routes.js')(app, passport);
 
-//displays our homepage
-app.get('/', function(req, res){
-  res.render('home', {user: req.user});
-});
-
-//displays our signup page
-app.get('/signin', function(req, res){
-  res.render('signin');
-});
-
-//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
-app.post('/local-reg', passport.authenticate('local-signup', {
-  successRedirect: '/',
-  failureRedirect: '/signin'
-  })
-);
-
-//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
-app.post('/login', passport.authenticate('local-signin', {
-  successRedirect: '/',
-  failureRedirect: '/signin'
-  })
-);
-
-//logs user out of site, deleting them from the session, and returns to homepage
-app.get('/logout', function(req, res){
-  var name = req.user.username;
-  console.log("LOGGIN OUT " + req.user.username)
-  req.logout();
-  res.redirect('/');
-  req.session.notice = "You have successfully been logged out " + name + "!";
-});
-
-// =====================================
-// GOOGLE ROUTES =======================
-// =====================================
-app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-
-// the callback after google has authenticated the user
-app.get('/auth/google/callback',
-  passport.authenticate('google', {
-    scope : ['profile'],
-    successRedirect : '/',
-    failureRedirect : '/signin'
-  })
-);
-
-// =====================================
-// FACEBOOK ROUTES =====================
-// =====================================
-app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email'] }));
-
-// the callback after google has authenticated the user
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {
-    successRedirect : '/',
-    failureRedirect : '/signin'
-  })
-);
 
 //===============socket.io============
 
@@ -251,15 +194,4 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-
-/*
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
-  //response.render('pages/index');
-});
-
-http.listen(app.get('port'), function() {
-  console.log('Node app is running on port num ', app.get('port'));
-});
-*/
 

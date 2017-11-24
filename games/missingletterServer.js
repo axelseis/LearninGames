568f446053ecabbe4e68b7f311408cb27a9f1235
Axel claver turiégano <axel@axflash.com>
2017-11-24 19:00:43 +0100

    fix: updated shuffleArray function

--- games/missingletterServer.js
+++ games/missingletterServer.js
@@ -107,10 +107,12 @@ MissingLetter.prototype = {
     }
   },
  
-  _shuffleArray: function(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+  _shuffleArray: function (array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
   },
 
